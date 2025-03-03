588961c8a04e3812d255d45bdcd32765b8f6fb62
Petr Molchanov <molc.pyotr@yandex.ru>
2025-03-03 03:17:21 -0300

    styles update

--- src/app/admin/ref/page.tsx
+++ src/app/admin/ref/page.tsx
@@ -5,7 +5,10 @@ import { RefLinkList } from "@/components/RefLink/RefLinkList";
 export default async function AdminPage() {
   return (
     <Page>
-      <div style={{ backgroundColor: "var(--tgui--secondary_bg_color)" }}>
+      <div
+        style={{ backgroundColor: "var(--tgui--secondary_bg_color)" }}
+        className={"h-[calc(100vh-62px)] overflow-auto"}
+      >
         <CreateRefLink />
         <RefLinkList />
       </div>
